43325ca4670061401dc2d07e866731be9b437661
argel <gel.raw@gmail.com>
2023-12-02 14:21:36 +1100

    part 2

--- day1/calibrate_documents_part2.py
+++ day1/calibrate_documents_part2.py
@@ -1,50 +1,32 @@
 import sys
 
-digits_dict = {
-    "zero" : "0",
-    "one" : "1",
-    "two" : "2",
-    "three" : "3",
-    "four" : "4",
-    "five" : "5",
-    "six" : "6",
-    "seven" : "7",
-    "eight" : "8",
-    "nine" : "9"
-}
+digits_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-def get_digits(line_from_list):
-    digits_word_index = []
-    digits_index = []
-    number = []
-    for num in digits_dict:
-        word_num = line_from_list.find(num)
-        if word_num != -1:
-            digits_word_index.append(word_num)
-    for i, c in enumerate(line_from_list):
+def create_first_digit_dict(line):
+    first_digit_dict = {}
+    for i in range(len(digits_list)):
+        word_index = line.find(digits_list[i])
+        if word_index != -1:
+            first_digit_dict.update({word_index:i})
+    for i, c in enumerate(line):
         if c.isdigit() == True:
-            digits_index.append(i)
-    numbers_index = digits_index + digits_word_index
-    numbers_index.sort()
-    if line_from_list[numbers_index[0]].isdigit():
-        first_digit = line_from_list[numbers_index[0]]
-        number.append(first_digit)
-    else:
-        for num in digits_dict:
-            word_num =line_from_list.find(num)
-            if word_num == numbers_index[0]:
-                number.append(digits_dict[num])
-                break
-    if line_from_list[numbers_index[-1]].isdigit():
-        last_digit = line_from_list[numbers_index[-1]]
-        number.append(last_digit)
-    else:
-        for num in digits_dict:
-            word_num =line_from_list.find(num)
-            if word_num == numbers_index[-1]:
-                number.append(digits_dict[num])
-                break
-    return number
+            first_digit_dict.update({i:int(c)})
+    min_value_key = min(first_digit_dict.keys())
+    min_value = first_digit_dict[min_value_key]
+    return str(min_value)
+
+def create_last_digit_dict(line):
+    last_digit_dict = {}
+    for i in range(len(digits_list)):
+        word_index = line.rfind(digits_list[i])
+        if word_index != -1:
+            last_digit_dict.update({word_index:i})
+    for i, c in enumerate(line):
+        if c.isdigit() == True:
+            last_digit_dict.update({i:int(c)})
+    max_value_key = max(last_digit_dict.keys())
+    max_value = last_digit_dict[max_value_key]
+    return str(max_value)
 
 with open(sys.argv[1], 'r') as file:
     lines = []
@@ -53,8 +35,8 @@ with open(sys.argv[1], 'r') as file:
         lines.append(line)
 sum = 0
 for line in lines:
-    number = get_digits(line)
-    combine_digits = number[0] + number[1]
-    sum = sum + int(combine_digits)
-
+    first = create_first_digit_dict(line)
+    last = create_last_digit_dict(line)
+    combined_digits = first+last
+    sum = sum + int(combined_digits)
 print(sum)
